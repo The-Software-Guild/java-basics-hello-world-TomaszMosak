e87f9a1833ea5ec959c7adefedcf2a7ce5e2349b
Tomasz Mosak <46677243+TomaszMosak@users.noreply.github.com>
2020-06-03 12:53:14 +0100

    Hello World using Netbeans IDE

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author Tomasz Mosak
+ * @date 01/06/2020
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("My name is Tomasz Mosak");
+    }
+}
